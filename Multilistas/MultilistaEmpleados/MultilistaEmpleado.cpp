ec910f08c92dda19a1a7a247df0465a9e14a9991
AlejandroPenagos84 <alejopenagos79@gmail.com>
2023-12-12 16:02:54 -0500

    Se pone bonito el codigo xd

--- Multilistas/MultilistaEmpleados/MultilistaEmpleado.cpp
+++ Multilistas/MultilistaEmpleados/MultilistaEmpleado.cpp
@@ -12,15 +12,38 @@ MultilistaEmpleado::MultilistaEmpleado()
 
 void MultilistaEmpleado::Agregar(Empleado &empleado)
 {
+    AgregarAArbol(empleado);
+    OrganizarDatos(empleado);
+    empleados.AddLast(empleado);
+}
 
+void MultilistaEmpleado::AgregarAArbol(Empleado &empleado)
+{
     std::string nombreCompleto = empleado.getNombre() + " " + empleado.getApellido();
     Nodo<std::string, Empleado> *nodo = arbolEmpleados->createNodo(nombreCompleto, empleado);
     arbolEmpleados->Insert(arbolEmpleados, nodo);
-
+}
+void MultilistaEmpleado::OrganizarDatos(Empleado &empleado)
+{
     GettersEmpleado<std::string> getterEmpleadoCiudadNacimiento = ConfigurarGettersEmpleado(&Empleado::getCiudadNacimiento, &Empleado::getAntCiudadNacimiento, &Empleado::getSigCiudadNacimiento);
     SettersEmpleado<std::string> setterEmpleadoCiudadNacimiento = ConfigurarSettersEmpleado(&Empleado::setCiudadNacimiento, &Empleado::setAntCiudadNacimiento, &Empleado::setSigCiudadNacimiento);
     OrganizarGenerico(cabeceraCiudadNacimiento, &empleado, getterEmpleadoCiudadNacimiento, setterEmpleadoCiudadNacimiento);
-    empleados.AddLast(empleado);
+
+    GettersEmpleado<std::string> getterEmpleadoPaisNacimiento = ConfigurarGettersEmpleado(&Empleado::getPaisNacimiento, &Empleado::getAntPaisNacimiento, &Empleado::getSigPaisNacimiento);
+    SettersEmpleado<std::string> setterEmpleadoPaisNacimiento = ConfigurarSettersEmpleado(&Empleado::setPaisNacimiento, &Empleado::setAntPaisNacimiento, &Empleado::setSigPaisNacimiento);
+    OrganizarGenerico(cabeceraPaisNacimiento, &empleado, getterEmpleadoPaisNacimiento, setterEmpleadoPaisNacimiento);
+
+    GettersEmpleado<std::string> getterEmpleadoCiudadResidencia = ConfigurarGettersEmpleado(&Empleado::getCiudadResidencia, &Empleado::getAntCiudadResidencia, &Empleado::getSigCiudadResidencia);
+    SettersEmpleado<std::string> setterEmpleadoCiudadResidencia = ConfigurarSettersEmpleado(&Empleado::setCiudadResidencia, &Empleado::setAntCiudadResidencia, &Empleado::setSigCiudadResidencia);
+    OrganizarGenerico(cabeceraCiudadResidencia, &empleado, getterEmpleadoCiudadResidencia, setterEmpleadoCiudadResidencia);
+
+    GettersEmpleado<std::string> getterEmpleadoPaisResidencia = ConfigurarGettersEmpleado(&Empleado::getPaisResidencia, &Empleado::getAntPaisResidencia, &Empleado::getSigPaisResidencia);
+    SettersEmpleado<std::string> setterEmpleadoPaisResidencia = ConfigurarSettersEmpleado(&Empleado::setPaisResidencia, &Empleado::setAntPaisResidencia, &Empleado::setSigPaisResidencia);
+    OrganizarGenerico(cabeceraPaisResidencia, &empleado, getterEmpleadoPaisResidencia, setterEmpleadoPaisResidencia);
+
+    GettersEmpleado<std::string> getterEmpleadoActividadLaboral = ConfigurarGettersEmpleado(&Empleado::getActividadLaboral, &Empleado::getAntActividadLaboral, &Empleado::getSigActividadLaboral);
+    SettersEmpleado<std::string> setterEmpleadoActividadLaboral = ConfigurarSettersEmpleado(&Empleado::setActividadLaboral, &Empleado::setAntActividadLaboral, &Empleado::setSigActividadLaboral);
+    OrganizarGenerico(cabeceraActividadLaboral, &empleado, getterEmpleadoActividadLaboral, setterEmpleadoActividadLaboral);
 }
 
 GettersEmpleado<std::string> MultilistaEmpleado::ConfigurarGettersEmpleado(std::string (Empleado::*atributo)() const, Empleado *(Empleado::*punteroAnt)() const, Empleado *(Empleado::*punteroSig)() const)
@@ -43,22 +66,18 @@ SettersEmpleado<std::string> MultilistaEmpleado::ConfigurarSettersEmpleado(void 
 
 void MultilistaEmpleado::OrganizarGenerico(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters)
 {
-    //* Si no existe un nodo con el dato especificado, eso significa que el dato no existe, por ello se agrega un nodo a un árbol
     if (arbolCabecera->findNodo((empleado->*getters.obtenerDato)()) == arbolCabecera->getNullNode())
         ManejarNodoNoExistente(arbolCabecera, empleado, getters, setters);
     else
-        //* Ya existe un nodo en el árbol con el dato del empleado
         ManejarNodoExistente(arbolCabecera, empleado, getters, setters);
 }
 
 void MultilistaEmpleado::ManejarNodoNoExistente(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters)
 {
     Cabecera<Empleado> cabEmpleado;
-
     cabEmpleado.primerDato = empleado;
     cabEmpleado.ultimoDato = NULL;
 
-    // Crea un nuevo nodo con la cabecera ya hecha y lo agrega al árbol
     Nodo<std::string, Cabecera<Empleado>> *nodo = arbolCabecera->createNodo((empleado->*getters.obtenerDato)(), cabEmpleado);
     arbolCabecera->Insert(arbolCabecera, nodo);
 }
@@ -67,20 +86,25 @@ void MultilistaEmpleado::ManejarNodoExistente(RBTree<std::string, Cabecera<Emple
 {
     Cabecera<Empleado> &cabEmpleado = arbolCabecera->findNodo((empleado->*getters.obtenerDato)())->Valor;
     if (cabEmpleado.ultimoDato == NULL)
-    {
-        cabEmpleado.ultimoDato = empleado;
-        (cabEmpleado.ultimoDato->*setters.setPunteroAnt)(cabEmpleado.primerDato);
-        (cabEmpleado.primerDato->*setters.setPunteroSig)(cabEmpleado.ultimoDato);
-        (cabEmpleado.ultimoDato->*setters.setPunteroSig)(NULL);
-    }
+        ManejarNodosExistenteUnDato(cabEmpleado, empleado, getters, setters);
     else
-    {
-        // Leer el codigo como set(Dato)
-        (cabEmpleado.ultimoDato->*setters.setPunteroSig)(empleado);
-        (empleado->*setters.setPunteroAnt)(cabEmpleado.ultimoDato);
-        cabEmpleado.ultimoDato = empleado;
-        (cabEmpleado.ultimoDato->*setters.setPunteroSig)(NULL);
-    }
+        ManejarNodoExistenteVariosDatos(cabEmpleado, empleado, getters, setters);
+}
+
+void MultilistaEmpleado::ManejarNodosExistenteUnDato(Cabecera<Empleado> &cabEmpleado, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters)
+{
+    cabEmpleado.ultimoDato = empleado;
+    (cabEmpleado.ultimoDato->*setters.setPunteroAnt)(cabEmpleado.primerDato);
+    (cabEmpleado.primerDato->*setters.setPunteroSig)(cabEmpleado.ultimoDato);
+    (cabEmpleado.ultimoDato->*setters.setPunteroSig)(NULL);
+}
+void MultilistaEmpleado::ManejarNodoExistenteVariosDatos(Cabecera<Empleado> &cabEmpleado, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters)
+{
+    // Leer el codigo como set(Dato)
+    (cabEmpleado.ultimoDato->*setters.setPunteroSig)(empleado);
+    (empleado->*setters.setPunteroAnt)(cabEmpleado.ultimoDato);
+    cabEmpleado.ultimoDato = empleado;
+    (cabEmpleado.ultimoDato->*setters.setPunteroSig)(NULL);
 }
 
 void MultilistaEmpleado::Eliminar(Empleado &empleado)
--- Multilistas/MultilistaEmpleados/MultilitaEmpleado.hpp
+++ Multilistas/MultilistaEmpleados/MultilitaEmpleado.hpp
@@ -7,10 +7,11 @@
 #define MULTILISTA_EMPLEADO_HPP
 
 #include "../../Librerias/ListaDoble.hpp"
-#include "../../Estructuras/Empleado.hpp"
 #include "../../Librerias/Arbol.hpp"
+#include "../../Estructuras/Empleado.hpp"
 #include "../../Estructuras/Cabecera.hpp"
 #include "../Multilista.hpp"
+
 #include <string>
 #include <iostream>
 
@@ -61,16 +62,27 @@ public:
     void Eliminar(Empleado &empleado) override;
     void Modificar(Empleado &empleado) override;
 
-    void OrganizarGenerico(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
-    void ManejarNodoNoExistente(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
-    void ManejarNodoExistente(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
-
-    GettersEmpleado<std::string> ConfigurarGettersEmpleado(std::string (Empleado::*atributo)() const, Empleado *(Empleado::*punteroAnt)() const, Empleado *(Empleado::*punteroSig)() const);
-    SettersEmpleado<std::string> ConfigurarSettersEmpleado(void (Empleado::*setAtributo)(const std::string &), void (Empleado::*setPunteroAnt)(Empleado *), void (Empleado::*setPunteroSig)(Empleado *));
+    // Getters
     RBTree<std::string, Cabecera<Empleado>> *getCabeceraCiudadNacimiento();
     RBTree<std::string, Cabecera<Empleado>> *getCabeceraPaisNacimiento();
     RBTree<std::string, Cabecera<Empleado>> *getCabeceraCiudadResidencia();
     RBTree<std::string, Cabecera<Empleado>> *getCabeceraPaisResidencia();
     RBTree<std::string, Cabecera<Empleado>> *getCabeceraActividadLaboral();
+
+private:
+    void AgregarAArbol(Empleado &empleado);
+
+    // Metodo para organizar los datos en las cabeceras
+    void OrganizarDatos(Empleado &empleado);
+    void OrganizarGenerico(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
+    void ManejarNodoNoExistente(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
+
+    void ManejarNodoExistente(RBTree<std::string, Cabecera<Empleado>> *arbolCabecera, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
+    void ManejarNodosExistenteUnDato(Cabecera<Empleado> &cabEmpleado, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
+    void ManejarNodoExistenteVariosDatos(Cabecera<Empleado> &cabEmpleado, Empleado *empleado, GettersEmpleado<std::string> getters, SettersEmpleado<std::string> setters);
+
+    // Metodos para obtener los getters y setters especificados
+    GettersEmpleado<std::string> ConfigurarGettersEmpleado(std::string (Empleado::*atributo)() const, Empleado *(Empleado::*punteroAnt)() const, Empleado *(Empleado::*punteroSig)() const);
+    SettersEmpleado<std::string> ConfigurarSettersEmpleado(void (Empleado::*setAtributo)(const std::string &), void (Empleado::*setPunteroAnt)(Empleado *), void (Empleado::*setPunteroSig)(Empleado *));
 };
 #endif
